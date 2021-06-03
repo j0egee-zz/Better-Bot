40035f2febc732005f6045d65a8a58458c145328
j0egee <josephguidici@gmail.com>
2021-06-02 21:36:34 -0400

    Added unmute command and times mute commands

--- commands/mute.js
+++ commands/mute.js
@@ -1,19 +1,28 @@
+const ms = require('ms');
+
 module.exports = {
     name: 'mute',
     description: "Mute a member in your server!",
     execute(message, args) {
         const target = message.mentions.users.first();
-        if(target){
-                let mainRole = message.guild.roles.cache.find(role => role.name === 'Verified Member');
-                let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+        if (target) {
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
-                let memberTarget= message.guild.members.cache.get(target.id);
+            let memberTarget = message.guild.members.cache.get(target.id);
 
-                memberTarget.roles.remove(mainRole.id);
+            if (!args[1]) {
                 memberTarget.roles.add(muteRole.id);
-                message.channel.send(`<@${memberTarget.user.id}> has been muted!`)
-        } else{
+                message.channel.send(`<@${memberTarget.user.id}> has been muted!`);
+                return
+            }
+            memberTarget.roles.add(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}!`);
+
+            setTimeout(function () {
+                memberTarget.roles.remove(muteRole.id);
+                }, ms(args[1]));
+        } else {
             message.channel.send('I cant seem to mute that member. Please re-check your command and try again.');
-         }
+        }
     }
 }
---
+++ commands/unmute.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'unmute',
+    description: "Unmute a member in your server!",
+    execute(message, args) {
+        const target = message.mentions.users.first();
+        if(target){
+                let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+
+                let memberTarget= message.guild.members.cache.get(target.id);
+
+                memberTarget.roles.remove(muteRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> has been unmuted!`)
+        } else{
+            message.channel.send('I cant seem to unmute that member. Please re-check your command and try again.');
+         }
+    }
+}
