a19552cd2561c2407f5880aa42523bf804c44029
j0egee <josephguidici@gmail.com>
2021-06-03 16:17:59 -0400

    Delete invite command

--- commands/invite.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'invite',
-    description: "Invite Better Bot to your own discord server!",
-    execute(message, args) {
-        message.channel.send('Invite me to your discord server! \nhttps://bit.ly/3g3gNzc');
-    }
-}
