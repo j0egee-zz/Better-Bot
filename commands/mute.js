350807e95ddb8bf8dcb64c8071feccb521ac234b
j0egee <josephguidici@gmail.com>
2021-06-03 21:24:32 -0400

    Fix mute log message

--- commands/mute.js
+++ commands/mute.js
@@ -5,6 +5,8 @@ module.exports = {
     description: "Mute a member in your server!",
     execute(message, args) {
         const target = message.mentions.users.first();
+
+            
         if (target) {
             let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
@@ -12,20 +14,21 @@ module.exports = {
 
             if (!args[1]) {
                 memberTarget.roles.add(muteRole.id);
-                memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}> has been muted by ...`)
+                memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}> has been muted!`);
                 return
             }
             memberTarget.roles.add(muteRole.id);
-            memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}> has been muted by ... for ${ms(ms(args[1]))}!`)
+            memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}!`)
 
             setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
-                memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}>'s mute has expired! Originally muted by ...`)
-                }, ms(args[1]));
-                
-            
+                memberTarget.guild.channels.cache.get('849850651218411530').send(`<@${memberTarget.user.id}>'s mute has expired!`)
+            }, ms(args[1]));
+
+
         } else {
             message.channel.send('I cant seem to mute that member. Please re-check your command and try again.');
+
         }
     }
 }
