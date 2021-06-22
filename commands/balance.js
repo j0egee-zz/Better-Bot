1f818f5669ce8e7bbc491f4984e6ebc8cb25e3ea
j0egee <josephguidici@gmail.com>
2021-06-22 10:56:25 -0400

    Better balance

--- commands/balance.js
+++ commands/balance.js
@@ -1,10 +1,9 @@
 module.exports = {
-    name: 'balance',
-    aliases: ['bal', 'coins', 'money'],
+    name: "balance",
+    aliases: ["bal", "bl"],
     permissions: [],
-    cooldown: 0.1,
-    description: "Check your coin balance!",
-    execute(client, message, args, Discord, profileData){
-        message.channel.send(`${message.author.username}, your current balance is *${profileData.coins}*, your bank balance is *${profileData.bank}*!`);
-    }
-}
+    description: "Check the user balance",
+    execute(client, message, cmd, args, Discord, profileData) {
+      message.reply(`Your current wallet balance is *${profileData.coins} coins*, and you bank balance is *${profileData.bank} coins*.`);
+    },
+  };
