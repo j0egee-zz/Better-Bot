0a099997e7bd275b5d5689a01e05a5fb629c44a5
j0egee <josephguidici@gmail.com>
2021-07-03 18:24:20 -0400

    Fix?

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,9 +1,9 @@
-module.exports = async (client) =>{
+module.exports = async (client, message, cmd, args, Discord, profileData) =>{
     const guild = client.guilds.cache.get('845366607080456262');
     setInterval(() =>{
         const memberCount = guild.memberCount
         const channel = guild.channels.cache.get('850089517783580702');
         channel.setName(`Members: ${memberCount.toLocaleString()}`);
-        console.log('Updating member count');
-    }, 600000);
+        console.log(`Updating member count. ${memberCount.toLocaleString()} members in the guild`);
+    }, 100);
 }
