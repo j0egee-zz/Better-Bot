ff1d69ae74e8e9e0b7f2f367d025af4834d1acf0
j0egee <josephguidici@gmail.com>
2021-07-05 20:27:57 -0400

    Create data command

---
+++ commands/createdata.js
@@ -0,0 +1,31 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: "createdata",
+    aliases: [],
+    permissions: ['ADMINISTRATOR'],
+    cooldown: 0,
+    description: "Manually create data for a user in the guild.",
+    async execute(client, message, cmd, args, Discord, profileData) {
+
+        var target = message.mentions.members.first();
+
+        if(!target)return message.channel.send('You must mention a user in this guild. **Warning! Do not create an account for a user that already has one! Check \`-userinfo\` to confirm!**');
+
+
+        let userData = await profileModel.findOne({ userID: target.id});
+
+        if(userData)return message.channel.send(`${target} already has data.`);
+        else{
+
+        let profile = await profileModel.create({
+            userID: target.id,
+            serverID: message.guild.id,
+            coins: 1000,
+            bank: 0,
+          });
+          profile.save();
+          message.channel.send(`${target} now has data in my system!`)
+    }
+}
+}
