9e54e1875aa9393047e2e64524498ac1923c60d1
j0egee <josephguidici@gmail.com>
2021-07-05 20:27:48 -0400

    Send error if no user data

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -11,12 +11,14 @@ module.exports = {
         
         const userData = await profileModel.findOne({ userID: pfpMember.id});
 
+        if(!userData)return message.channel.send('An error has occured. This user is most likely not in my system yet. Contect the bot developor to get this user manually added.')
+
         const embed = new Discord.MessageEmbed()
             .setColor('FADF2E')
             .setTimestamp(Date.now())
             .setAuthor(`Username - ${pfpMember.user.tag}\nNickName - ${pfpMember.nickname}\nUserID - ${pfpMember.user.id}`)
             .setImage(pfpMember.user.displayAvatarURL({ dynamic: true }))
-            .setDescription(`Coins: ${userData.coins}\nBank: ${userData.bank}`)
+            .setDescription(`Coins - ${userData.coins}\nBank - ${userData.bank}\nJoined on - ${pfpMember.joinedAt}\nCreated on - ${pfpMember.user.createdAt}`)
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png");
 
         message.channel.send(embed);
