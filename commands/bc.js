14090673210e565cb816b9aba5c233ca40118698
j0egee <josephguidici@gmail.com>
2021-07-08 16:18:29 -0400

    Now can mention channel instead of neeing the ID

--- commands/bc.js
+++ commands/bc.js
@@ -4,10 +4,10 @@ module.exports = {
     permissions: ["KICK_MEMBERS"],
     description: 'Send a message to a channel in the server!',
     execute(client, message, cmd, args, Discord, profileData){
-        const bcChannel = message.guild.channels.cache.get(args[0])
+        const bcChannel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first()
         if(!bcChannel)return message.channel.send('You must say the channel ID you wish to send your annoucment in.')
 
-        let messageArgs = message.content.split(`-bc ${bcChannel.id}`).join("")
+        let messageArgs = message.content.split(`-bc ${bcChannel}`).join("")
 
         const embed = new Discord.MessageEmbed()
         .setTitle('New annoucment!')
