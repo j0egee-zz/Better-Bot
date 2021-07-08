bf0dcbcac61c150579dd3306f14aa506700af73d
j0egee <josephguidici@gmail.com>
2021-07-08 14:45:38 -0400

    Better format

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on("message", async(message, guild, Discord) =>{
         .setTimestamp(Date.now())
         .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
         .setAuthor(`New DM from ${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({ dynamic: true }))
-        .setDescription(`\`\`\`${message.content}\`\`\``)
+        .setDescription(`${message.content}`)
 
         const DMC = client.channels.cache.get('849850651218411530')
         DMC.send(dmEmbed)
