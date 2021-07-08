727f4c71e4f686f461431b1cc311c934b6874dd5
j0egee <josephguidici@gmail.com>
2021-07-08 16:23:35 -0400

    Edit error message

--- commands/bc.js
+++ commands/bc.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Send a message to a channel in the server!',
     execute(client, message, cmd, args, Discord, profileData){
         const bcChannel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first()
-        if(!bcChannel)return message.channel.send('You must say the channel ID you wish to send your annoucment in.')
+        if(!bcChannel)return message.channel.send('You must say the channel you wish to send your annoucment in.')
 
         let messageArgs = message.content.split(`-bc ${bcChannel}`).join("")
 
