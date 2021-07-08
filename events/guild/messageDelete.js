d3467c8b621a0f9924028ce4fbaf797842b6a1df
j0egee <josephguidici@gmail.com>
2021-07-08 13:11:02 -0400

    Better format

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -6,7 +6,7 @@ module.exports = async(message, Discord) =>{
     .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
     .setTitle('Message delete')
     .setDescription(`${message.author}'s message was deleted in ${message.channel}`)
-    .addField(`Message content`,message.content,true)
+    .addField(`Message content`,message.content)
 
     let logs = message.guild.channels.cache.get('849850651218411530')
     logs.send(embed)
