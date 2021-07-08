e59c37a8c8a5b94338036aaac221aad73d73af81
j0egee <josephguidici@gmail.com>
2021-07-08 13:10:54 -0400

    Add users roles

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,11 +7,15 @@ module.exports = {
     description: "Get the user info for a user in the guild!",
     async execute(client, message, cmd, args, Discord, profileData) {
         if (message.content.startsWith('-userinfo')) {
-        var pfpMember = message.mentions.members.first() || message.member;
+        var pfpMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         
         const userData = await profileModel.findOne({ userID: pfpMember.id});
 
-        if(!userData)return message.channel.send('An error has occured. This user is most likely not in my system yet. Contect the bot developor to get this user manually added.')
+        if(!userData)return message.channel.send('An error has occured. This user is most likely not in my system yet. Please use the \`-createdata\` command!');
+        
+        const pfpMemberRoles = pfpMember.roles.cache
+        .filter((roles) => roles.id !== message.guild.id)
+        .map((role) => role.toString());
 
         const embed = new Discord.MessageEmbed()
             .setColor('FADF2E')
@@ -21,7 +25,8 @@ module.exports = {
             .setImage(pfpMember.user.displayAvatarURL({ dynamic: true }))
             .setDescription(`Coins - ${userData.coins}\n\n`
             + `Joined on - ${pfpMember.joinedAt.toLocaleDateString()}, ${pfpMember.joinedAt.toLocaleTimeString()}\n\n`
-            + `Created on - ${pfpMember.user.createdAt.toLocaleDateString()}, ${pfpMember.user.createdAt.toLocaleTimeString()}\n\n`);
+            + `Created on - ${pfpMember.user.createdAt.toLocaleDateString()}, ${pfpMember.user.createdAt.toLocaleTimeString()}\n\n`
+            + `Roles - ${pfpMemberRoles}`);
 
         message.channel.send(embed);
         message.channel.send(`Here is ${pfpMember}'s user info.`);
