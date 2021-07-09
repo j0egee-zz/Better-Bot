71362667817778e6a4db9744afe79b4bdd8fae4e
j0egee <josephguidici@gmail.com>
2021-07-09 13:52:05 -0400

    Ticket close via command

---
+++ commands/delticket.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "delticket",
+    aliases: ['closeticket', 'deleteticket'],
+    permissions: ["KICK_MEMBERS"],
+    cooldown: 0.1,
+    description: "Pay a member in the server some coins!",
+    execute(client, message, cmd, args, Discord, profileData) {
+
+        if(!message.channel.name.startsWith('ticket'))return message.channel.send('You can only run this command inside of a ticket.')
+
+        const deleteEmbed = new Discord.MessageEmbed()
+        .setColor('FADF2E')
+        .setTimestamp(Date.now())
+        .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+        .setTitle(`${message.channel.name}`)
+        .setDescription(`Has been deleted by ${message.author.tag}`);
+
+
+        message.channel.send("Deleting this channel in 5 seconds!");
+        setTimeout(() => message.channel.delete(), 5000);
+        message.guild.channels.cache.get('860961443393830912').send(deleteEmbed)
+        }
+        
+    }
--- commands/ticket.js
+++ commands/ticket.js
@@ -44,50 +44,7 @@ console.log(channel.parentID);
         .setFooter(`Bot created by j0egee#0001`, message.author.displayAvatarURL({ dynamic: true }))
         .setDescription(`<#${channel.id}>`)
     )
-    const reactionMessage = await channel.send("Ill let @everyone know that there is a new ticket now.");
+    channel.send("Ill let @everyone know that there is a new ticket now.");
 
-    try {
-        await reactionMessage.react("🔒");
-        await reactionMessage.react("⛔");
-      } catch (err) {
-        channel.send("Error sending emojis!");
-        throw err;
-      }
-  
-      const collector = reactionMessage.createReactionCollector(
-        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("KICK_MEMBERS"),
-        { dispose: true }
-      );
-  
-      collector.on("collect", (reaction, user) => {
-
-        const lockEmbed = new Discord.MessageEmbed()
-        .setColor('FADF2E')
-        .setTimestamp(Date.now())
-        .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
-        .setTitle(`${message.author.username}'s ticket`)
-        .setDescription(`Has been locked by ${user.username}`);
-
-        const deleteEmbed = new Discord.MessageEmbed()
-        .setColor('FADF2E')
-        .setTimestamp(Date.now())
-        .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
-        .setTitle(`${message.author.username}'s ticket`)
-        .setDescription(`Has been deleted by ${user.username}`);
-
-        switch (reaction.emoji.name) {
-          case "🔒":
-            channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
-            message.guild.channels.cache.get('860961443393830912').send(lockEmbed)
-            break;
-          case "⛔":
-            channel.send("Deleting this channel in 5 seconds!");
-            setTimeout(() => channel.delete(), 5000);
-            message.guild.channels.cache.get('860961443393830912').send(deleteEmbed)
-            break;
-        }
-    });
-    message.channel 
-    setTimeout(() => message.delete(), 3000);
 }
 }
