f6f3c0899d6cb74f3663bac17ab6d85f8a59597b
j0egee <josephguidici@gmail.com>
2021-07-12 18:57:20 -0400

    Undo fix

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js')
-const Discord = require('discord.js')
 
 module.exports = async (message, Discord) => {
     let embed = new MessageEmbed()
--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js')
-const Discord = require('discord.js')
 
 module.exports = async (oldMessage, newMessage, Discord) => {
     let embed = new MessageEmbed()
