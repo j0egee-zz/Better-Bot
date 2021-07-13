bde7e8d41325b224231b13079f70d8470ca26209
j0egee <josephguidici@gmail.com>
2021-07-12 22:53:11 -0400

    Add partnership

---
+++ commands/buypartnership.js
@@ -0,0 +1,30 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: 'buypartnership',
+    permissions: [],
+    cooldown: 0.1,
+    description: "Buy a partnership ad.",
+    async execute(client, message, cmd, args, Discord, profileData) {
+        let userData = await profileModel.findOne({ userID: message.author.id, serverID: message.guild.id })
+        const user = message.author
+        const partnerRole = message.guild.roles.cache.find(role => role.name === "Partner");
+
+        if (message.member.roles.cache.has('864337595009990687')) return message.channel.send('You already bought this.')
+
+        if (userData.coins < 25000) return message.channel.send('You do not have enough coins to buy this.')
+        else {
+            await profileModel.findOneAndUpdate({
+                userID: message.author.id,
+                serverID: message.guild.id
+            }, {
+                $set: {
+                    coins: userData.coins - 25000,
+                },
+            }
+            );
+            await message.guild.members.cache.get(user.id).roles.add(partnerRole)
+            message.channel.send('You have auccessfully bought partneship! Contact <@473850297702285322> to hear the rules and claim your ad.')
+        }
+    }
+}
--- commands/shop.js
+++ commands/shop.js
@@ -9,7 +9,7 @@ module.exports = {
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
             .setTitle(`Shop`)
             .addField(`-buyColors`, `*5,000 coins*\nBuy access to change your server color.`)
-            .addField(`-buy`)
+            .addField(`-buyPartnership`, `*25,000 coins*\n Buy a partnership ad, for more info see <@473850297702285322>`)
 
         message.channel.send(embed)
     }
