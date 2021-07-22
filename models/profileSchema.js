a3a7e63106231912992b8dfb7642474abe3c0a58
j0egee <josephguidici@gmail.com>
2021-07-22 12:39:16 -0400

    Remove bank

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const profileSchema = new mongoose.Schema({
   userID: { type: String, require: true, unique: true },
   serverID: { type: String, require: true },
-  coins: { type: Number, default: 1000 },
-  bank: { type: Number },
+  coins: { type: Number, default: 1000 }
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
