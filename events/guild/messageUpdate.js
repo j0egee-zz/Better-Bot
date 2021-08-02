26bfe2773e7da90f70dc1fbc1f3b37cf04165d40
j0egee <josephguidici@gmail.com>
2021-08-02 11:45:33 -0400

    Jump to message

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -7,6 +7,7 @@ module.exports = async (oldMessage, newMessage, Discord) => {
         .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
         .setTitle('Message update')
         .setDescription(`${oldMessage.author}'s message was updated in ${oldMessage.channel}`)
+        .addField('Jump to message', `[CLICK HERE](https://discord.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.id})`)
         .addField(`Old message`, oldMessage.content)
         .addField(`New message`, newMessage.content)
 
