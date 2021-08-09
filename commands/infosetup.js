6e768048ebb8cbaa51e6d4245c8768b846adf23a
j0egee <josephguidici@gmail.com>
2021-08-09 16:25:20 -0400

    Add link

--- commands/infosetup.js
+++ commands/infosetup.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTimestamp(Date.now())
             .setDescription(`In this server you can send your own ad's for new discord servers, a discord bot, or anything else.\nJust follow these rules and you will be all good.\n\n`
                 + `**Rule 1 -** Be nice to all members!\n**Rule 2 -** Do not spam!!\n**Rule 3 -** Use the appropriate channel when sending any message!\n**Rule 4 -** Keep all content PG-13!\n**Rule 5 -** Follow Discord ToS!\n**Rule 6 -** English only!\n\n`
-                + `If you have any questions or problems feel free to ping myself or any other staff member, or make a ticket.`)
+                + `If you have any questions or problems feel free to ping myself or any other staff member, or make a ticket.\n\n[Offical Discord link](https://discord.gg/paVctwJM3R)`)
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
             .setColor('FADF2E')
 
