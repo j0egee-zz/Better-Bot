ec9741b38534809b1c02b28b2a7f32d462180a17
j0egee <josephguidici@gmail.com>
2021-08-12 19:02:27 -0400

    Downgrade to fix crash

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const fs = require('fs');
