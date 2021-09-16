313a12b76cc0783ec2ecffc232bc480db6ed8f23
j0egee <josephguidici@gmail.com>
2021-09-16 15:31:33 -0400

    Fix wrong role cache

--- commands/purple.js
+++ commands/purple.js
@@ -10,8 +10,8 @@ module.exports = {
         if (!message.member.roles.cache.has('863161193781788683')) return message.channel.send('You must buy access to colors first. Use \`-shop\`.');
         else {
 
-            if (!message.member.roles.cache.has('863160849183014912')) return message.guild.members.cache.get(message.author.id).roles.add(purpleColorRole), message.reply('You now have the purple color role.');
-            if (message.member.roles.cache.has('863160849183014912')) return message.guild.members.cache.get(message.author.id).roles.remove(purpleColorRole), message.reply('You no longer have the purple color role.');
+            if (!message.member.roles.cache.has('863160999263600660')) return message.guild.members.cache.get(message.author.id).roles.add(purpleColorRole), message.reply('You now have the purple color role.');
+            if (message.member.roles.cache.has('863160999263600660')) return message.guild.members.cache.get(message.author.id).roles.remove(purpleColorRole), message.reply('You no longer have the purple color role.');
 
         }
     }
