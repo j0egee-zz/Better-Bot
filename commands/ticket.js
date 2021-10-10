696420fb20d5564315c7bf86a92989b67a314926
j0egee <josephguidici@gmail.com>
2021-10-10 19:42:19 -0400

    Better tickets

--- commands/ticket.js
+++ commands/ticket.js
@@ -2,11 +2,19 @@ module.exports = {
     name: 'ticket',
     aliases: [],
     permissions: [],
-    cooldown: 0.5,
+    cooldown: 0.05,
     description: 'Creates a ticket!',
     async execute(client, message, cmd, args, Discord, profileData) {
 
         if (message.member.roles.cache.has('866922287072673812')) return message.reply('You are banned from making tickets in this server!');
+
+        const firstChannel = message.guild.channels.cache.find(channels => channels.name === `ticket-${message.author.username}`);
+
+        if(firstChannel) return message.reply(`You can only have 1 ticket opened at a time. You currently have <#${firstChannel.id}> opened.`)
+
+        let reason = message.content.split('-ticket ').join("");
+
+        if(!args[0]) return message.reply('Please be sure to include your reason for creating the ticket.');
 
         const ownerRole = message.guild.roles.cache.find(role => role.name === 'Owner');
         const modRole = message.guild.roles.cache.find(role => role.name === 'Moderator');
@@ -24,17 +32,20 @@ module.exports = {
 
         channel.send(new Discord.MessageEmbed()
             .setColor('fadf2e')
+            .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({ dynamic: true }))
             .setTimestamp(Date.now())
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
-            .setDescription(`Hey there <@${message.author.id}>\nThank you for contacting our support team! Please start off by explaining what issue you are having.`)
+            .setDescription(`Thank you for contacting our support team!\n\n**Reason: "${reason}"**\n\nPlease use this time to explain anything you feel important, and someone will be with you shortly.`)
         )
+
+        message.delete();
 
         const createEmbed = new Discord.MessageEmbed()
             .setColor('FADF2E')
             .setTimestamp(Date.now())
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
             .setTitle(`${message.author.username}`)
-            .setDescription(`Has created a ticket ${channel}`);
+            .setDescription(`Has created a ticket ${channel}\n\n**${reason}**`);
 
         message.guild.channels.cache.get('863157013567373332').send(createEmbed)
 
@@ -42,7 +53,7 @@ module.exports = {
             .setColor('fadf2e')
             .setTimestamp(Date.now())
             .setAuthor('I made the ticket for you!')
-            .setFooter(`Bot created by j0egee#0001`, message.author.displayAvatarURL({ dynamic: true }))
+            .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
             .setDescription(`<#${channel.id}>`)
         )
         channel.send("Ill let @everyone know that there is a new ticket now.");
--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -1,19 +1,23 @@
 module.exports = {
-    name: "delticket",
-    aliases: ['closeticket', 'deleteticket'],
+    name: "ticketclose",
+    aliases: [ ],
     permissions: ["KICK_MEMBERS"],
-    cooldown: 0.1,
+    cooldown: 0.01,
     description: "Pay a member in the server some coins!",
     execute(client, message, cmd, args, Discord, profileData) {
 
-        if (!message.channel.name.startsWith('ticket')) return message.channel.send('You can only run this command inside of a ticket.')
+        if (!message.channel.name.startsWith('ticket')) return message.channel.send('You can only run this command inside of a ticket.');
+
+        let reason = message.content.split('-ticketclose ').join("");
+
+        if(!args[0]) return message.reply('Please be sure to include your solution for closing this ticket.');
 
         const deleteEmbed = new Discord.MessageEmbed()
             .setColor('FADF2E')
             .setTimestamp(Date.now())
             .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
             .setTitle(`${message.channel.name}`)
-            .setDescription(`Has been deleted by ${message.author.tag}`);
+            .setDescription(`Has been deleted by ${message.author.tag}\n\n**${reason}**`);
 
 
         message.channel.send("Deleting this channel in 5 seconds!");
