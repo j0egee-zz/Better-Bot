d5154824a1601721b3981ee9bcb759b2a902b9ca
j0egee <josephguidici@gmail.com>
2021-10-14 17:51:45 -0400

    Add responce if target has all

--- commands/history.js
+++ commands/history.js
@@ -9,10 +9,10 @@ module.exports = {
     description: "Check a members punishment history!",
     async execute(client, message, cmd, args, Discord, profileData) {
 
-        const target = message.mentions.users.first() 
+        const target = message.mentions.users.first()
 
-        if(!target) return message.reply(`Be sure to mention the user. If you want to search by the users ID use \`-historyid\``);
-       
+        if (!target) return message.reply(`Be sure to mention the user. If you want to search by the users ID use \`-historyid\``);
+
         const muteData = await muteLogs.findOne({ userID: target.id, guildID: message.guild.id })
         const kickData = await kickLogs.findOne({ userID: target.id, guildID: message.guild.id })
         const banData = await banLogs.findOne({ userID: target.id, guildID: message.guild.id })
@@ -148,5 +148,46 @@ module.exports = {
             message.channel.send(banEmbed)
         }
 
+        if (muteData && kickData && banData) {
+
+            const muteEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(target.tag)
+                .setTitle(`Mute history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, muteData.reason)
+                .addField(`Staff member:`, muteData.staffTag)
+                .addField(`Current:`, muteData.current)
+                .addField(`Expires:`, `<t:${((muteData.expires) / 1000).toFixed()}>`)
+                .addField(`Given:`, `<t:${((muteData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(muteEmbed)
+
+            const kickEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(target.tag)
+                .setTitle(`Kick history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, kickData.reason)
+                .addField(`Staff member:`, kickData.staffTag)
+                .addField(`Given:`, `<t:${((kickData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(kickEmbed)
+
+            const banEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(target.tag)
+                .setTitle(`Ban history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, banData.reason)
+                .addField(`Staff member:`, banData.staffTag)
+                .addField(`Current:`, banData.current)
+                .addField(`Given:`, `<t:${((banData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(banEmbed)
+        }
     }
 }
--- commands/historyid.js
+++ commands/historyid.js
@@ -9,8 +9,8 @@ module.exports = {
     description: "Check a members punishment history (By user id)!",
     async execute(client, message, cmd, args, Discord, profileData) {
 
-        const target = args[0] 
-       
+        const target = args[0]
+
         const muteData = await muteLogs.findOne({ userID: target, guildID: message.guild.id })
         const kickData = await kickLogs.findOne({ userID: target, guildID: message.guild.id })
         const banData = await banLogs.findOne({ userID: target, guildID: message.guild.id })
@@ -94,7 +94,7 @@ module.exports = {
             const muteEmbed = new Discord.MessageEmbed()
                 .setColor('FADF2E')
                 .setTimestamp(Date.now())
-                .setAuthor(`<@${target}>`)
+                .setAuthor(muteData.userTag)
                 .setTitle(`Mute history`)
                 .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
                 .addField(`Reason:`, muteData.reason)
@@ -120,6 +120,48 @@ module.exports = {
         }
         if (!muteData && kickData && banData) {
             message.channel.send('This user has not been muted.')
+            const kickEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(kickData.userTag)
+                .setTitle(`Kick history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, kickData.reason)
+                .addField(`Staff member:`, kickData.staffTag)
+                .addField(`Given:`, `<t:${((kickData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(kickEmbed)
+
+            const banEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(banData.userTag)
+                .setTitle(`Ban history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, banData.reason)
+                .addField(`Staff member:`, banData.staffTag)
+                .addField(`Current:`, banData.current)
+                .addField(`Given:`, `<t:${((banData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(banEmbed)
+        }
+
+        if (muteData && kickData && banData) {
+
+            const muteEmbed = new Discord.MessageEmbed()
+                .setColor('FADF2E')
+                .setTimestamp(Date.now())
+                .setAuthor(muteData.userTag)
+                .setTitle(`Mute history`)
+                .setFooter(`Bot created by j0egee#0001`, "https://cdn.discordapp.com/attachments/845366607080456265/861746867008569384/Untitled_Artwork_3.png")
+                .addField(`Reason:`, muteData.reason)
+                .addField(`Staff member:`, muteData.staffTag)
+                .addField(`Current:`, muteData.current)
+                .addField(`Expires:`, `<t:${((muteData.expires) / 1000).toFixed()}>`)
+                .addField(`Given:`, `<t:${((muteData.date) / 1000).toFixed()}>`)
+
+            message.channel.send(muteEmbed)
+
             const kickEmbed = new Discord.MessageEmbed()
                 .setColor('FADF2E')
                 .setTimestamp(Date.now())
