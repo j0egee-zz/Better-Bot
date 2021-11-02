0a0f88f6677da89d3eb57a6eef9b2ad2ebe81085
j0egee <josephguidici@gmail.com>
2021-11-01 23:17:11 -0400

    Reword

--- commands/accept.js
+++ commands/accept.js
@@ -11,7 +11,7 @@ module.exports = {
 
         message.channel.send(`<@${target.id}> has been accepted by <@${message.author.id}>!`);
 
-        target.user.send(`Congratulations! Your staff application has been accepted by ${message.author.username}. He is our guide to becoming a staff member, <link here>. If you are avaliable please make your way into the general voice channel and someone will drag you up! If you are not avaliable to VC now please contact ${message.author.tag} and it can get rearanged to another time.`);
+        target.user.send(`Congratulations! Your staff application has been accepted by ${message.author.tag}. Please send them a PM to set a time to meet and train.`);
         message.delete()
 
     }
