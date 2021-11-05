c67e4ad1a610aba9010195d624967994dedfdd4e
j0egee <josephguidici@gmail.com>
2021-11-05 15:00:37 -0400

    Add !args reply

--- commands/permissions.js
+++ commands/permissions.js
@@ -1,20 +1,40 @@
 module.exports = {
     name: "permissions",
-    aliases: ['perms'],
+    aliases: ['perms', 'perm'],
     permissions: ['BAN_MEMBERS'],
     cooldown: 0,
     description: "Check/Edit soemones perms.",
     async execute(client, message, cmd, args, Discord, profileData) {
 
         const target = message.mentions.members.first();
+        if (!target) return message.channel.send(`Be sure to mention the user.`);
+        if (!args[1]) return message.channel.send(`Please use the correct arguments. Options:\`info\`, \`add\`, or \`remove\``);
+        if (args[1] !== 'info') {
+            if (args[1] !== 'add') {
+                if (args[1] !== 'remove') {
+                    message.channel.send('Please use the correct arguments. Options:\`info\`, \`add\`, or \`remove\`');
+                }
+            }
+        }
+        if (!args[2]) return message.channel.send(`Please use the correct arguments. Options:\`tickets\`, \`apply\`, \`suggestions\`,\`economy\`, or \`emoji\``);
+        if (args[2] !== 'tickets') {
+            if (args[2] !== 'apply') {
+                if (args[2] !== `suggestions`) {
+                    if (args[2] !== 'economy') {
+                        if (args[2] !== 'emoji') {
+                            message.channel.send('Please use the correct arguments. Options:\`tickets\`, \`apply\`, \`suggestions\`,\`economy\`, or \`emoji\`');
+                        }
+                    }
+                }
+            }
+        }
 
         const ticketRole = message.guild.roles.cache.find(role => role.name === 'Ticket Banned');
         const suggestionRole = message.guild.roles.cache.find(role => role.name === 'Suggestion Banned');
         const economyRole = message.guild.roles.cache.find(role => role.name === 'Economy Blacklist');
         const emojiRole = message.guild.roles.cache.find(role => role.name === 'Emoji Blocked');
         const applyRole = message.guild.roles.cache.find(role => role.name === 'Application Blocked');
-        
-        904930227237572688
+
         if (args[1] === 'info') {
             if (args[2] === 'tickets') {
                 if (target.roles.cache.has('866922287072673812')) {
@@ -52,90 +72,89 @@ module.exports = {
                 }
             }
         }
-        if(args[1] === 'remove') {
-            if(args[2] === 'tickets') {
-                if(target.roles.cache.has('866922287072673812')) {
+        if (args[1] === 'remove') {
+            if (args[2] === 'tickets') {
+                if (target.roles.cache.has('866922287072673812')) {
                     message.reply('This user is already blocked from making tickets.')
                 } else {
-                target.roles.add(ticketRole);
-                message.reply('This user can no longer make tickets.')
+                    target.roles.add(ticketRole);
+                    message.reply('This user can no longer make tickets.')
                 }
             }
-            if(args[2] === 'apply') {
-                if(target.roles.cache.has('904930227237572688')) {
+            if (args[2] === 'apply') {
+                if (target.roles.cache.has('904930227237572688')) {
                     message.reply('This user is already blocked from applying.')
                 } else {
-                target.roles.add(applyRole);
-                message.reply('This user can no longer apply.')
+                    target.roles.add(applyRole);
+                    message.reply('This user can no longer apply.')
                 }
             }
-            if(args[2] === 'suggestions') {
-                if(target.roles.cache.has('866922320324329472')) {
+            if (args[2] === 'suggestions') {
+                if (target.roles.cache.has('866922320324329472')) {
                     message.reply('This user is already blocked from making suggestions')
                 } else {
-                target.roles.add(suggestionRole);
-                message.reply('This user can no longer make suggestions.')
+                    target.roles.add(suggestionRole);
+                    message.reply('This user can no longer make suggestions.')
                 }
             }
-            if(args[2] === 'economy') {
-                if(target.roles.cache.has('898604884528603136')) {
+            if (args[2] === 'economy') {
+                if (target.roles.cache.has('898604884528603136')) {
                     message.reply('This user is already blocked from the server economy.')
                 } else {
-                target.roles.add(economyRole);
-                message.reply('This user can no longer use the server economy.')
+                    target.roles.add(economyRole);
+                    message.reply('This user can no longer use the server economy.')
                 }
             }
-            if(args[2] === 'emoji') {
-                if(target.roles.cache.has('899394022932635659')) {
+            if (args[2] === 'emoji') {
+                if (target.roles.cache.has('899394022932635659')) {
                     message.reply('This user is already blocked from sending emojis/stickers.')
                 } else {
-                target.roles.add(emojiRole);
-                message.reply('This user can no longer send emojis/stickers.')
+                    target.roles.add(emojiRole);
+                    message.reply('This user can no longer send emojis/stickers.')
                 }
             }
         }
-        if(args[1] === 'add') {
-            if(args[2] === 'tickets') {
-                if(!target.roles.cache.has('866922287072673812')) {
+        if (args[1] === 'add') {
+            if (args[2] === 'tickets') {
+                if (!target.roles.cache.has('866922287072673812')) {
                     message.reply('This user is not blocked from making tickets.')
                 } else {
-                target.roles.remove(ticketRole);
-                message.reply('This user can now make tickets.')
+                    target.roles.remove(ticketRole);
+                    message.reply('This user can now make tickets.')
                 }
             }
-            if(args[2] === 'apply') {
-                if(!target.roles.cache.has('904930227237572688')) {
+            if (args[2] === 'apply') {
+                if (!target.roles.cache.has('904930227237572688')) {
                     message.reply('This user is not blocked from applying.')
                 } else {
-                target.roles.remove(applyRole);
-                message.reply('This user can now apply.')
+                    target.roles.remove(applyRole);
+                    message.reply('This user can now apply.')
                 }
             }
-            if(args[2] === 'suggestions') {
-                if(!target.roles.cache.has('866922320324329472')) {
+            if (args[2] === 'suggestions') {
+                if (!target.roles.cache.has('866922320324329472')) {
                     message.reply('This user is not blocked from making suggestions')
                 } else {
-                target.roles.remove(suggestionRole);
-                message.reply('This user can now make suggestions.')
+                    target.roles.remove(suggestionRole);
+                    message.reply('This user can now make suggestions.')
                 }
             }
-            if(args[2] === 'economy') {
-                if(!target.roles.cache.has('898604884528603136')) {
+            if (args[2] === 'economy') {
+                if (!target.roles.cache.has('898604884528603136')) {
                     message.reply('This user is not blocked from the server economy.')
                 } else {
-                target.roles.remove(economyRole);
-                message.reply('This user can now use the server economy.')
+                    target.roles.remove(economyRole);
+                    message.reply('This user can now use the server economy.')
                 }
             }
-            if(args[2] === 'emoji') {
-                if(!target.roles.cache.has('899394022932635659')) {
+            if (args[2] === 'emoji') {
+                if (!target.roles.cache.has('899394022932635659')) {
                     message.reply('This user is not blocked from sending emojis/stickers.')
                 } else {
-                target.roles.remove(emojiRole);
-                message.reply('This user can now send emojis/stickers.')
+                    target.roles.remove(emojiRole);
+                    message.reply('This user can now send emojis/stickers.')
                 }
             }
         }
-            
     }
 }
